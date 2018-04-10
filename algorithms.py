b3007f0de8d9c4adb7369806c700bf253303724b
charlesdognin <charles.dognin@polytechnique.edu>
2018-04-10 17:05:37 +0200

    first commit

---
+++ algorithms.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+
+## SVRG
+
+def svrg(itr1,, itr2, stepsize, dim):
+    theta_0 = np.random.randn()
+    for k in range(itr1):
+        g = np.mean()
+        for t in range(itr2):
+            pass
+        return None
+
+## SVRG2
+
+def svrg2(itr1,, itr2, stepsize, dim):
+    return None
+
+## CURVATURE MATCHING
+
+def curvature_matching(f, stepsize):
+    return None
+
+## ACTION MATCHING
+
+def action_matching(f, stepsize):
+    return None
